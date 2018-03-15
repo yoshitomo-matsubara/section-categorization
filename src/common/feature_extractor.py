19ed32d2d9e683e3f921cbcaa7fa12e7f1223edb
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2018-03-15 00:39:50 -0700

    added mindf option

--- src/common/feature_extractor.py
+++ src/common/feature_extractor.py
@@ -22,12 +22,13 @@ def extract_header_features(file_line_matrix):
     return header_feature_mat
 
 
-def extract_features(input_file_path_list, transformed_vectorizer):
+def extract_features(input_file_path_list, min_df, transformed_vectorizer):
     file_line_list = file_util.read_files_as_line_list(input_file_path_list, start_idx=1)
     file_line_matrix = file_util.read_files(input_file_path_list)
     header_feature_mat = extract_header_features(file_line_matrix)
+    min_df = 1 if min_df < 0 else min_df
     if transformed_vectorizer is None:
-        vectorizer = CountVectorizer(min_df=50, stop_words='english', binary=True)
+        vectorizer = CountVectorizer(min_df=min_df, stop_words='english', binary=True)
         vectorizer.fit(file_line_list)
         return vectorizer, vectorizer.fit_transform(file_line_list), header_feature_mat
     return transformed_vectorizer, transformed_vectorizer.transform(file_line_list), header_feature_mat
@@ -49,10 +50,10 @@ def detect_abstract_file(input_file_path_list):
     return np.array(file_label_path_mat), non_abstract_flags
 
 
-def extract(base_input_dir_path, data_type, vectorizer, base_output_dir_path):
+def extract(base_input_dir_path, data_type, min_df, vectorizer, base_output_dir_path):
     input_dir_path = os.path.join(base_input_dir_path, data_type, config.RAW)
     input_file_path_list = file_util.get_file_list(input_dir_path, is_recursive=True)
-    transformed_vectorizer, feature_mat, header_feature_mat = extract_features(input_file_path_list, vectorizer)
+    transformed_vectorizer, feature_mat, header_feature_mat = extract_features(input_file_path_list, min_df, vectorizer)
     file_label_path_mat, non_abstract_flags = detect_abstract_file(input_file_path_list)
     output_dir_path = os.path.join(base_output_dir_path, data_type, config.EXTRACTED)
     output_file_path = os.path.join(output_dir_path, config.FEATURE_FILE_NAME)
@@ -72,13 +73,14 @@ def extract(base_input_dir_path, data_type, vectorizer, base_output_dir_path):
 
 
 def main(args):
-    transformed_vectorizer = extract(args.input, config.TRAINING, None, args.output)
-    extract(args.input, config.VALIDATION, transformed_vectorizer, args.output)
-    extract(args.input, config.TEST, transformed_vectorizer, args.output)
+    transformed_vectorizer = extract(args.input, config.TRAINING, args.mindf, None, args.output)
+    extract(args.input, config.VALIDATION, args.mindf, transformed_vectorizer, args.output)
+    extract(args.input, config.TEST, args.mindf, transformed_vectorizer, args.output)
 
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description=os.path.basename(__file__))
     arg_parser.add_argument('-input', required=True, help='[input] dataset dir path')
+    arg_parser.add_argument('-mindf', required=False, type=int, default=200, help='[param] min document frequency')
     arg_parser.add_argument('-output', required=True, help='[output] output dir path')
     main(arg_parser.parse_args())
